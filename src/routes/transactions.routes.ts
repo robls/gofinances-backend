db8a74a09c564d09218a04207aa913420110d587
Joao Roberto <joao.r.lopesdasilva@gmail.com>
2020-04-19 07:25:12 -0300

    :art: Added create,list and delete transaction routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,73 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+import uploadConfig from '../config/upload';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const balance = await transactionsRepository.getBalance();
+  const transactions = await transactionsRepository.find({
+    relations: ['category'],
+  });
+
+  // CONSIDER USING A DIFFERENT METHOD, SINCE THIS ONE IS NOT
+  // COST EFFICIENT IN BIGGER REQUESTS
+  // transactions.map(transaction => {
+  //   delete transaction.created_at;
+  //   delete transaction.updated_at;
+  //   delete transaction.category.created_at;
+  //   delete transaction.category.updated_at;
+  //   return transaction;
+  // });
+
+  return response.json({
+    transactions,
+    balance,
+  });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, type, value, category } = request.body;
+
+  const createTransactionService = new CreateTransactionService();
+
+  const transaction = await createTransactionService.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransactionService = new DeleteTransactionService();
+  await deleteTransactionService.execute(id);
+  return response.status(200).json({ message: 'Ok' });
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('csvFile'),
+  async (request, response) => {
+    const { filename } = request.file;
+
+    const importTransactionsService = new ImportTransactionsService();
+
+    const transactions = await importTransactionsService.execute(filename);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
